ff9d5b8d20352324719edd90f3090fe06a6e1e45
Asmir Pljakic <92116999+asmirpljakic@users.noreply.github.com>
2023-05-08 08:38:13 +0200

    react-navbar

---
+++ src/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { links, social } from './data';
+
+const Sidebar = () => {
+  return (
+    <>
+      <h4>Sidebar</h4>
+      <div className='links-container show-container'>
+        <ul className='links'>
+          <div className='links-container show-container'>
+            <ul className='links'>
+              {links.map(link => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
